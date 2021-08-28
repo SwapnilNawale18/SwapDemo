2cf6f68673dfc6de89a4c52c09c5070244fd581c
Swapnil Nawale <swapnilnawale18@gmail.com>
2021-08-28 19:20:44 +0530

    Add project files.

---
+++ SwapDemo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace SwapDemo
+{
+    class Swap
+    {
+        void swapping(ref int a, ref int b)
+        {
+            //Using 3rd variable
+            int c = a;
+            a = b;
+            b = c;
+            /*a += b;
+            b = a - b;
+            a -= b;*/
+        }
+        static void Main(string[] args)
+        {
+            int x, y;
+            Console.Write("Enter value of x: ");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter value of y: ");
+            y = Convert.ToInt32(Console.ReadLine());
+            Swap s = new Swap();
+            s.swapping(ref x, ref y);
+            Console.WriteLine("After Swapping");
+            Console.WriteLine("Value of x is: " + x);
+            Console.WriteLine("Value of y is: " + y);
+        }
+    }
+}
